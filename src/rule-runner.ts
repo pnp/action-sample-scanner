4c2ff46c204050123d236462234af3208fe3c632
Patrick Rodgers <patrick.rodgers@microsoft.com>
2022-10-03 15:38:30 -0400

    'testing'

--- src/rule-runner.ts
+++ src/rule-runner.ts
@@ -63,7 +63,7 @@ export async function runner(scanPaths: string[]) {
                     result = "";
                 }
 
-                scanSummaryRow.push(`![warning]("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2048 2048'%3E%3Cpath d='M1920 2048H0L960 128l960 1920zm-896-384H896v128h128v-128zm0-128V896H896v640h128z' fill='%23333333'%3E%3C/path%3E%3C/svg%3E")${result}`);
+                scanSummaryRow.push(`![warning]("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2048 2048'%3E%3Cpath d='M1920 2048H0L960 128l960 1920zm-896-384H896v128h128v-128zm0-128V896H896v640h128z' fill='%23333333'%3E%3C/path%3E%3C/svg%3E") ${result} :rocket:`);
 
             } catch (e) {
 
